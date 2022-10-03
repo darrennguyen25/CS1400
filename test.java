1b201ea8938dfd8102f7ac1fa8b129e77e610f43
Darren Nguyen <darrennguyen@cpp.edu>
2022-10-03 00:16:16 -0700

    Added comment above main method

--- test.java
+++ test.java
@@ -4,7 +4,8 @@ public class test {
     public static int lengthOne = exampleVariableOne.length;
     // returns the length of the columns in the array
     public static int lengthTwo = exampleVariableOne[0].length;
-  
+    
+    //Demo run
     public static void main(String[] args) {
       System.out.println(lengthOne);
       System.out.println(lengthTwo);
